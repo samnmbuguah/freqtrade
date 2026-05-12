e575c5a22bcc102284e827d6e7324da2f7627688
Samuel Nyambura <samnmbuguah@gmail.com>
2026-05-12 23:20:27 +0200

    revert  ohlcv_candle_limit  back to default

--- freqtrade/exchange/binance.py
+++ freqtrade/exchange/binance.py
@@ -52,7 +52,7 @@ class Binance(Exchange):
         "supports_demo_trading": False,
     }
     _ft_has_futures: FtHas = {
-        "ohlcv_candle_limit": 1499,
+        "ohlcv_candle_limit": 499,
         "funding_fee_candle_limit": 1000,
         "stoploss_order_types": {"limit": "stop", "market": "stop_market"},
         "stoploss_blocks_assets": False,  # Stoploss orders do not block assets
